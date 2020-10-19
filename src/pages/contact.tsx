07066f6c3e73a81a3ce06db404063b2128673bf6
sushidesu <hugemerge@gmail.com>
2020-10-19 09:58:16 +0000

    feat: Add contact page

---
+++ src/pages/contact.tsx
@@ -0,0 +1,10 @@
+import { Layout } from "components/Layout"
+
+const Contact = (): JSX.Element => (
+  <Layout>
+    <h1>お問い合わせ</h1>
+    <p>example@example.com までご連絡ください。</p>
+  </Layout>
+)
+
+export default Contact
