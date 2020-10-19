1e3b14cd914de8288eaaa88611d97e6f86ece265
sushidesu <hugemerge@gmail.com>
2020-10-19 09:06:30 +0000

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div>
+        <h1>wasurena-shop</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Home
