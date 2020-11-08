2c52376858e9de6ab53e3d9e7e5153495e9f22e3
sushidesu <hugemerge@gmail.com>
2020-11-08 07:47:42 +0000

    Add news page

---
+++ src/components/common/InstaCard.tsx
@@ -0,0 +1,100 @@
+import { styled } from "plugins/emotion"
+
+export const InstaCard = styled.div`
+  position: relative;
+  border-radius: 4px;
+  border: 1px solid #f1f1f1;
+  box-shadow: ${({ theme }) => theme.shadow};
+  padding: 2rem 2.4rem 1.4rem 2.4rem;
+  width: fit-content;
+  margin: 0 auto;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+export const IconWrapper = styled.div`
+  position: absolute;
+  top: 0.6rem;
+  right: 0.6rem;
+  font-size: 1.2rem;
+  color: ${({ theme }) => theme.color.secondary};
+`
+
+export const ImageWrapper = styled.div`
+  position: relative;
+  width: 150px;
+  padding-bottom: 150px;
+  border-radius: 50%;
+  background-color: ${({ theme }) => theme.color.white};
+
+  & > img {
+    position: absolute;
+    object-fit: contain;
+    width: 100%;
+    height: 100%;
+  }
+
+  // instagram border
+  &::before {
+    content: "";
+    position: absolute;
+    top: -0.6rem;
+    left: -0.6rem;
+    z-index: -1;
+    width: 150px;
+    height: 150px;
+    padding: 0.6rem;
+    box-sizing: content-box;
+    border-radius: 50%;
+    background: linear-gradient(235deg, #d62976 0%, #fa7e1e 70%, #feda75 100%);
+  }
+  &::after {
+    content: "";
+    position: absolute;
+    top: -0.3rem;
+    left: -0.3rem;
+    z-index: -1;
+    width: 150px;
+    height: 150px;
+    padding: 0.3rem;
+    box-sizing: content-box;
+    border-radius: 50%;
+    background-color: white;
+  }
+`
+
+export const Title = styled.h2`
+  margin: 1.6rem 0 0 0;
+  color: ${({ theme }) => theme.color.primary};
+  font-size: 1.2rem;
+`
+
+export const ScreenName = styled.span`
+  color: ${({ theme }) => theme.color.secondary};
+`
+
+type Props = {
+  href: string
+}
+
+export const ExternalLink: React.FC<Props> = ({ href, children }) => (
+  <LinkButton href={href} target="_blank" rel="noopener noreferrer">
+    {children}
+  </LinkButton>
+)
+
+const LinkButton = styled.a`
+  margin-top: 1.2rem;
+  ${({ theme }) => `
+    display: inline-block;
+    background-color: ${theme.color.accent};
+    color: ${theme.color.white};
+    text-decoration: none;
+    font-weight: bold;
+    padding: .5rem 1rem;
+    border-radius: 4px;
+  `}
+`
--- src/pages/news.tsx
+++ src/pages/news.tsx
@@ -1,12 +1,51 @@
+import { styled } from "plugins/emotion"
+import { FaInstagram } from "react-icons/fa"
 import { Layout } from "components/Layout"
 import { Container } from "components/Container"
+import {
+  InstaCard,
+  IconWrapper,
+  ImageWrapper,
+  Title,
+  ScreenName,
+  ExternalLink,
+} from "components/common/InstaCard"
+
+const insta_url = "https://instagram.com/sobo_sapporo"
+const rel = "noopener noreferrer"
 
 const News = (): JSX.Element => (
   <Layout>
     <Container>
-      <h1>お知らせ</h1>
+      <Section>
+        <h1>お知らせ</h1>
+        <p>
+          最新情報は
+          <a target="_blank" href={insta_url} rel={rel}>
+            公式インスタグラム
+          </a>
+          または<a>ワスレナ商店公式LINE</a>にて随時配信しております。
+        </p>
+      </Section>
+      <div>
+        <InstaCard>
+          <IconWrapper>
+            <FaInstagram />
+          </IconWrapper>
+          <ImageWrapper>
+            <img src="/images/sobo-logo.png" />
+          </ImageWrapper>
+          <Title>蒼氓(そうぼう)</Title>
+          <ScreenName>sobo_sappro</ScreenName>
+          <ExternalLink href={insta_url}>最新情報を確認する</ExternalLink>
+        </InstaCard>
+      </div>
     </Container>
   </Layout>
 )
+
+const Section = styled.section`
+  padding: 2rem 0;
+`
 
 export default News
